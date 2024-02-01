80cb9d803c2286352422484af82d92082fe3b350
Winry4 <rebecamolinaprados@gmail.com>
2024-02-01 11:31:15 +0100

    Change to useNavigate

--- src/pages/ProtectedPage.js
+++ src/pages/ProtectedPage.js
@@ -1,17 +1,15 @@
-import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
-import { HomePage } from '../pages';
+import React, { useEffect } from "react";
+import {useNavigate } from "react-router-dom";
 
 export const ProtectedPage = () => {
-    const isAuthed = false;
-    return isAuthed
-    ?(
+    const isAuthed = true;
+    const navigate = useNavigate();
+    useEffect(() => {
+        if(!isAuthed) {
+            navigate('/');
+        }
+    });
+    return (
         <h1>Only authed users should be able to see this</h1>
-    ) : (
-        <Routes>
-            <Route path="/" element={<HomePage/>} />
-            <Route path="/protected" element={<Navigate replace to="/" />} />
-        </Routes>
-    )
-    ;
+    );
 }
